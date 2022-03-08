9c57e200e8fe5f68bfae482717b0f51a85b8d7c4
linhongzhou <3145526007@qq.com>
2022-03-08 15:04:16 +0800

    update site

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220308.1454';
+const cacheName = 'chirpy-20220308.1503';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
